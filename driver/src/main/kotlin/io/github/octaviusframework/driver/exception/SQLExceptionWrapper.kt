3b4665651dcaad785b22ef702bfba02d210aae01
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2026-07-15 17:59:15 +0200

    docs: kdoc

--- driver/src/main/kotlin/io/github/octaviusframework/driver/exception/SQLExceptionWrapper.kt
+++ driver/src/main/kotlin/io/github/octaviusframework/driver/exception/SQLExceptionWrapper.kt
@@ -2,4 +2,13 @@ package io.github.octaviusframework.driver.exception
 
 import java.sql.SQLException
 
+/**
+ * A wrapper class that adapts an [OctaviusException] into a standard [SQLException].
+ *
+ * This wrapper is necessary for integration with standard JDBC components like connection pools
+ * (e.g., HikariCP) which rely on intercepting [SQLException] to analyze connection state,
+ * evict dead connections, and properly interpret the `SQLState`.
+ *
+ * @property wrappedException The original [OctaviusException] that is being wrapped.
+ */
 internal class SQLExceptionWrapper(val wrappedException: OctaviusException) : SQLException(wrappedException.message, wrappedException.sqlState)
--- driver/src/main/kotlin/io/github/octaviusframework/driver/jdbc/JdbcInterfacesExtensions.kt
+++ driver/src/main/kotlin/io/github/octaviusframework/driver/jdbc/JdbcInterfacesExtensions.kt
@@ -8,6 +8,13 @@ import java.util.*
 import javax.sql.DataSource
 
 // DriverManager
+/**
+ * Establishes a new [OctaviusSession] using the specified [url] and [info] properties.
+ *
+ * @param url A database url of the form `jdbc:subprotocol:subname`.
+ * @param info A list of arbitrary string tag/value pairs as connection arguments.
+ * @return An [OctaviusSession] instance.
+ */
 fun getOctaviusSession(
     url: String,
     info: Properties
@@ -16,6 +23,14 @@ fun getOctaviusSession(
     return OctaviusSessionImpl(conn, conn.unwrapToOctavius())
 }
 
+/**
+ * Establishes a new [OctaviusSession] using the specified [url], [user], and [password].
+ *
+ * @param url A database url of the form `jdbc:subprotocol:subname`.
+ * @param user The database user on whose behalf the connection is being made.
+ * @param password The user's password.
+ * @return An [OctaviusSession] instance.
+ */
 fun getOctaviusSession(
     url: String,
     user: String, password: String
@@ -25,29 +40,61 @@ fun getOctaviusSession(
 }
 
 // DataSource
+/**
+ * Unwraps this [DataSource] to its underlying [OctaviusDataSource] instance.
+ *
+ * @return The underlying [OctaviusDataSource].
+ */
 fun DataSource.unwrapToOctavius(): OctaviusDataSource {
     return this.unwrap(OctaviusDataSource::class.java)
 }
 
+/**
+ * Unwraps this [DataSource] to an instance of the specified type [T].
+ *
+ * @return The underlying object of type [T].
+ */
 inline fun <reified T> DataSource.unwrap(): T {
     return this.unwrap(T::class.java)
 }
 
+/**
+ * Retrieves a new [OctaviusSession] from this [DataSource].
+ *
+ * @return An [OctaviusSession] instance.
+ */
 fun DataSource.getOctaviusSession(): OctaviusSession {
     val conn = this.getConnection()
     return OctaviusSessionImpl(conn, conn.unwrapToOctavius())
 }
 
+/**
+ * Retrieves a new [OctaviusSession] from this [DataSource] using the specified credentials.
+ *
+ * @param username The database user on whose behalf the connection is being made.
+ * @param pass The user's password.
+ * @return An [OctaviusSession] instance.
+ */
 fun DataSource.getOctaviusSession(username: String, pass: String): OctaviusSession {
     val conn = this.getConnection(username, pass)
     return OctaviusSessionImpl(conn, conn.unwrapToOctavius())
 }
 
 // Connection
+/**
+ * Unwraps this [Connection] to an instance of the specified type [T].
+ *
+ * @return The underlying object of type [T].
+ */
 inline fun <reified T : Any> Connection.unwrap(): T {
     return this.unwrap(T::class.java)
 }
 
+/**
+ * Unwraps this [Connection] to its underlying [OctaviusConnection] instance.
+ *
+ * @return The underlying [OctaviusConnection].
+ */
 internal fun Connection.unwrapToOctavius(): OctaviusConnection {
     return this.unwrap(OctaviusConnection::class.java)
 }
