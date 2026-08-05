8e1ced854346d3e30877717124cbab645de97ff1
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2026-08-05 18:24:30 +0200

    feat: TypeException test

--- driver/src/main/kotlin/io/github/octaviusframework/driver/exception/TypeException.kt
+++ driver/src/main/kotlin/io/github/octaviusframework/driver/exception/TypeException.kt
@@ -4,10 +4,7 @@ enum class TypeExceptionMessage {
     TYPE_NOT_FOUND,
     NOT_A_CONTAINER,
     MISSING_CODEC,
-    ATTRIBUTE_NOT_FOUND,
     ANONYMOUS_RECORD_NOT_SUPPORTED,
-    UNKNOWN_OID,
-    UNSUPPORTED_OID,
     NESTED_PGTYPED_NOT_ALLOWED
 }
 
@@ -32,9 +29,6 @@ private fun generateDeveloperMessage(messageEnum: TypeExceptionMessage): String 
         TypeExceptionMessage.TYPE_NOT_FOUND -> "The specified type was not found in the TypeRegistry."
         TypeExceptionMessage.NOT_A_CONTAINER -> "The type with the specified OID is not a valid container type (Composite, Array, etc.)."
         TypeExceptionMessage.MISSING_CODEC -> "Missing codec for the specific OID when parsing or serializing."
-        TypeExceptionMessage.ATTRIBUTE_NOT_FOUND -> "Requested attribute/column was not found in the composite type."
         TypeExceptionMessage.ANONYMOUS_RECORD_NOT_SUPPORTED -> "PostgreSQL does not support receiving anonymous composite types (record) as input parameters."
-        TypeExceptionMessage.UNKNOWN_OID -> "The specified OID is unknown to the TypeRegistry."
-        TypeExceptionMessage.UNSUPPORTED_OID -> "The specified OID is known but not supported."
         TypeExceptionMessage.NESTED_PGTYPED_NOT_ALLOWED -> "Nested PgTyped is not allowed. You cannot wrap a PgTyped instance within another PgTyped."
     }
