f53f0bf70632729c65655caa5e90d8a44684be03
PolskiAnonim <115878440+PolskiAnonim@users.noreply.github.com>
2026-08-08 15:17:37 +0200

    feat: dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.dokka.gradle.DokkaExtension
+import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.dokka)
 }
 
 allprojects {
@@ -14,7 +17,30 @@ allprojects {
     }
 }
 
+dependencies {
+    dokka(projects.driver)
+    dokka(projects.hikari)
+    dokka(projects.driverSpringIntegration)
+}
+
 subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
+    extensions.configure<DokkaExtension> {
+        moduleName.set(name)
+
+        dokkaSourceSets.configureEach {
+            documentedVisibilities.set(
+                setOf(
+                    VisibilityModifier.Public,
+                    VisibilityModifier.Protected,
+                    VisibilityModifier.Internal
+                )
+            )
+            skipEmptyPackages.set(true)
+        }
+    }
+
     plugins.withId("org.jetbrains.kotlin.jvm") {
         extensions.configure<KotlinJvmProjectExtension> {
             jvmToolchain(25)
